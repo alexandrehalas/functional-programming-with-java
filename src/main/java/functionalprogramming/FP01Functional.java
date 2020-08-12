d6e18895aac18113ed8abed27393c11921c51a22
Alexandre Halas <alexandrehalas@gmail.com>
2020-08-11 21:22:28 -0300

    Functional Programming 01

---
+++ src/main/java/functionalprogramming/FP01Functional.java
@@ -0,0 +1,21 @@
+package functionalprogramming;
+
+import java.util.List;
+
+public class FP01Functional {
+
+	public static void main(String[] args) {
+		
+		printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		
+	}
+
+	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream().forEach(FP01Functional::print);
+	}
+
+	private static void print(int number) {
+		System.out.println(number);
+	}
+	
+}
---
+++ src/main/java/functionalprogramming/FP01Structured.java
@@ -0,0 +1,21 @@
+package functionalprogramming;
+
+import java.util.List;
+
+public class FP01Structured {
+
+	public static void main(String[] args) {
+		
+		printAllNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+		
+	}
+
+	private static void printAllNumbersInListStructured(List<Integer> numbers) {
+		for (Integer number : numbers) {
+			System.out.println(number);
+		}
+	}
+
+	
+	
+}
