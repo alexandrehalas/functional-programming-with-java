fa25e6dd1666011bff396ffcd62c2663c4624fa0
Alexandre Halas <alexandrehalas@gmail.com>
2020-08-12 20:24:40 -0300

    Functional Programming 04
     - predicates
     - functions
     - consumers

---
+++ src/main/java/functionalprogramming/FP03FunctionalInterfaces.java
@@ -0,0 +1,21 @@
+package functionalprogramming;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FP03FunctionalInterfaces {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+		Predicate<Integer> isEvenNumber = number -> number%2 == 0;
+		Function<Integer, Integer> squareFunction = number -> number * number;
+		Consumer<Integer> sysoutConsumer = System.out::println;
+		numbers.stream()
+		.filter(isEvenNumber)
+		.map(squareFunction)
+		.forEach(sysoutConsumer);		
+	}
+	
+}
