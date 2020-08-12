f7185d33577812faa8290a3a2a45c17a1d29eb0b
Alexandre Halas <alexandrehalas@gmail.com>
2020-08-11 21:55:57 -0300

    Functional Programming
     - Map

--- src/main/java/functionalprogramming/FP01Functional.java
+++ src/main/java/functionalprogramming/FP01Functional.java
@@ -9,6 +9,7 @@ public class FP01Functional {
 		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
 		printAllNumbersInListFunctional(numbers);
 		printEvenNumbersInListFunctional(numbers);
+		printSquaresOfEvenNumbersInListFunctional(numbers);
 		
 	}
 
@@ -24,4 +25,12 @@ public class FP01Functional {
 			.forEach(System.out::println);
 	}
 
+	private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+		System.out.println("SQUARES OF EVEN NUMBERS:");
+		numbers.stream()
+			.filter(number -> number%2 == 0)
+			.map(number -> number * number)
+			.forEach(System.out::println);
+	}
+	
 }
